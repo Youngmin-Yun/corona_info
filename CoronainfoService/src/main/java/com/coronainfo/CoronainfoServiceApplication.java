6dbd061f4043d10c8aa08d91b7e91503a5b397d3
Youngmin-Yun <lauas15@naver.com>
2021-08-09 14:40:01 +0900

    added base codes

---
+++ CoronainfoService/src/main/java/com/coronainfo/CoronainfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coronainfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronainfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronainfoServiceApplication.class, args);
+	}
+
+}
